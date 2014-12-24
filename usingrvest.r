cadb6127d63b94397acd95bdf35d12419ecd68a3
macuser <tmthyjames@gmail.com>
2014-12-24 15:08:56 -0600

    getting numbers from bing

--- usingrvest.r
+++ usingrvest.r
@@ -15,8 +15,8 @@ firmslist <- list()
 locationlist <- list()
 mylist <- list()
 
-what <- readline("what? ")
-where <- readline("where? ")
+what <- 'it'
+where <- 'nashville'
 
 url <- "http://www.indeed.com/jobs?"
 q <- "q="
@@ -33,26 +33,73 @@ count <- as.numeric(gsub(",","",count))
 searchlimit <- if(count<1001) count else 1001
 
 i=0
-while (i<searchlimit) 
-{
-url = "http://www.indeed.com/jobs?q=it&l=nashville"
-start = "&start="
-inc = as.character(i)
-newurl = gsub(" ","",paste(url, start, inc))
-request <- html(newurl)
-jobs <- getinfo(request, "#resultsCol .jobtitle")
-jobslist$JOBS <- jobs
-firms <- getinfo(request, ".company")
-firmslist$FIRMS <- firms
-location <- getinfo(request, ".location")
-locationlist$LOCATION <- location
-mylist$JOBS <- jobslist$JOBS
-mylist$FIRMS <- firmslist$FIRMS
-mylist$LOCATION <- locationlist$LOCATION
-try({
-  write.table(mylist, file="/Users/macuser/Desktop/my.csv", row.names=FALSE, sep=",", append=TRUE, col.names=FALSE)
-})
-i <- i+10
+while (i<searchlimit) {
+  url = "http://www.indeed.com/jobs?q=it&l=nashville"
+  start = "&start="
+  inc = as.character(i)
+  newurl = gsub(" ","",paste(url, start, inc))
+  request <- html(newurl)
+  firms <- getinfo(request, ".company")
+  # firmslist$FIRMS <- firms
+  jobs <- getinfo(request, "#resultsCol .jobtitle")
+  jobs <- gsub("\n","",jobs)
+  # jobslist$JOBS <- jobs
+  location <- getinfo(request, ".location")
+  location <- gsub("[0-9]","",location)
+  location <- gsub("\\(.*\\)","",location)
+  # locationlist$LOCATION <- location
+  mylist$FIRMS <- firms
+  mylist$JOBS <- jobs
+  mylist$LOCATION <- location
+
+  # for (firm in firms) {
+  #   for (loc in location) {
+  #     firm <- gsub(" ","+",firm)
+  #     loc <- gsub(", ","+",loc)
+  #     bingurl <- 'http://www.bing.com/searchq?='
+  #     conturl <- paste(bingurl,firm,loc,sep="")
+  #     req <- html(conturl)
+  #     numbers <- gregexpr('[0-9]{3}[\\.-][0-9]{3}[\\.-][0-9]{4}', req)
+  #     print(numbers)
+  #   }
+  # }
+
+  try({
+    write.table(mylist, file="/Users/macuser/Desktop/my.csv", row.names=FALSE, sep=",", append=TRUE, col.names=FALSE)
+  })
+  i <- i+10
 }
+
+
+
+
+
+
+
+
+# mydata <- read.csv('Desktop/my.csv', header=FALSE)
+# colnames(mydata) <- c('FIRMS','JOBS','LOCATION')
+
+# for (i in mydata$FIRMS) {
+#   for (j in mydata$LOCATION) {
+
+#     url <- paste('https://www.bing.com/searchq?q=',gsub(" ","+",i),gsub(" ","+",j))
+#   }
+# }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
