19c8cbd896b6ea639fa3ededde087a4d172f6bc8
macuser <tmthyjames@gmail.com>
2014-12-24 11:00:19 -0600

    and try statement

--- usingrvest.r
+++ usingrvest.r
@@ -1,13 +1,28 @@
+#!/usr/bin/env Rscript
+
 # r script that grabs job titles, firm names, and locations from indeed.com
 
 require(rvest)
+
+getinfo <- function(page, tags) {
+  page %>%
+  html_nodes(tags) %>%
+  html_text()
+}
 
 jobslist <- list()
 firmslist <- list()
 locationlist <- list()
 mylist <- list()
 
-url <- "http://www.indeed.com/jobs?q=it&l=nashville"
+what <- readline("what? ")
+where <- readline("where? ")
+
+url <- "http://www.indeed.com/jobs?"
+q <- "q="
+l <- "&l="
+url <- paste(url,q,what,l,where,sep="")
+
 request <- html(url)
 count <- request %>%
   html_nodes("#searchCount") %>%
@@ -18,29 +33,26 @@ count <- as.numeric(gsub(",","",count))
 searchlimit <- if(count<1001) count else 1001
 
 i=0
-while (i<30) 
+while (i<searchlimit) 
 {
 url = "http://www.indeed.com/jobs?q=it&l=nashville"
 start = "&start="
 inc = as.character(i)
 newurl = gsub(" ","",paste(url, start, inc))
 request <- html(newurl)
-jobs <- request %>%
-  html_nodes("#resultsCol .jobtitle") %>%
-  html_text()
-  jobslist$JOBS <- jobs
-firms <- request %>%
-  html_nodes(".company") %>%
-  html_text()
-  firmslist$FIRMS <- firms
-location <- request %>%
-  html_nodes(".location") %>%
-  html_text()
-  locationlist$LOCATION <- location
+jobs <- getinfo(request, "#resultsCol .jobtitle")
+jobslist$JOBS <- jobs
+firms <- getinfo(request, ".company")
+firmslist$FIRMS <- firms
+location <- getinfo(request, ".location")
+locationlist$LOCATION <- location
 mylist$JOBS <- jobslist$JOBS
 mylist$FIRMS <- firmslist$FIRMS
 mylist$LOCATION <- locationlist$LOCATION
-write.table(mylist, file="my.csv", row.names=FALSE, sep=",", append=TRUE, col.names=FALSE)
+try({
+  write.table(mylist, file="/Users/macuser/Desktop/my.csv", row.names=FALSE, sep=",", append=TRUE, col.names=FALSE)
+})
 i <- i+10
 }
+
 
